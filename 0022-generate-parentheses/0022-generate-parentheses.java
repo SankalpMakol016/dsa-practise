cb5e3a0cf80a5cb2ea2dea63d9a70ba450665f23
Sankalp Makol <sankalp.sumit.makol@gmail.com>
2026-08-21 21:17:55 +0530

    Time: 1 ms (85.81%), Space: 44 MB (96.61%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    void backtrack(List<String> ans, StringBuilder sb,
+                   int open, int close, int n) {
+
+        if (sb.length() == 2 * n) {
+            ans.add(sb.toString());
+            return;
+        }
+
+        // We can always add '(' if we haven't used all of them
+        if (open < n) {
+            sb.append('(');
+            backtrack(ans, sb, open + 1, close, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        // ')' only if it won't make the sequence invalid
+        if (close < open) {
+            sb.append(')');
+            backtrack(ans, sb, open, close + 1, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
